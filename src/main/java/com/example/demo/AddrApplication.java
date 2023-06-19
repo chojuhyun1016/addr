063768af4922146a9dfe1b5e4373758409d2c96f
Cho Juhyun <chojuhyun1016@gmail.com>
2023-06-19 17:21:09 +0900

    20230619 02

--- src/main/java/com/example/demo/AddrApplication.java
+++ src/main/java/com/example/demo/AddrApplication.java
@@ -1,33 +1,14 @@
 package com.example.demo;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class AddrApplication extends SpringBootServletInitializer{
-
-	@Override 
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) { 
-		return application.sources(AddrApplication.class);
-	}
+public class AddrApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AddrApplication.class, args);
 	}
-
-    @PostConstruct
-    public void initialize(){
-
-    }
-    
-    @PreDestroy
-    public void preDestroy() {
-
-    }    
 }
